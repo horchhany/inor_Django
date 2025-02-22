7c9a909b5e72b5e9f74a67367e9505cd9800f650
kiki1681680 <houcnn@gmail.com>
2025-02-22 18:58:21 +0700

    Second push

---
+++ inor_App/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from inor_App.models import Movie  # Replace YourModel with the name of your model..
+# Register your models here.
+admin.site.register(Movie)  # Replace YourModel with the name of your model..
--- inor_DiNor/settings.py
+++ inor_DiNor/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inor_App',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
