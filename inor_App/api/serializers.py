a25dcd87cbcb2da0bf1a784c3bdc6ccae981186d
kiki1681680 <houcnn@gmail.com>
2025-02-25 18:38:52 +0700

    update REST

--- inor_App/api/serializers.py
+++ inor_App/api/serializers.py
@@ -3,9 +3,16 @@ from inor_App.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=2000)
-    avtive = serializers.BooleanField(default=True)
+    name = serializers.CharField()
+    description = serializers.CharField()
+    active = serializers.BooleanField()
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
--- inor_App/api/views.py
+++ inor_App/api/views.py
@@ -5,24 +5,37 @@ from inor_App.api.serializers import MovieSerializer
 
 @api_view(['GET', 'POST'])
 def movie_list(request):
+
     if request.method == 'GET':
         movies = Movie.objects.all()
-        serializer = MovieSerializer(movies)
+        serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
+    
     if request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data)
         else:
-            return Response(serializer.errors, status=400)
+            return Response(serializer.errors)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    try:
+    if request.method == 'GET':
         movie = Movie.objects.get(pk=pk)
-    except Movie.DoesNotExist:
-        return Response(status=404)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
     
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=204)
